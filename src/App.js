66c3eb3df1a8bf3e2cca5fd82645525d3f9c75a7
rahulrajqi <rahul.raj@quantuminventions.com>
2022-02-24 10:44:06 +0530

    feature: completed the configuration for tailwind

---
+++ src/App.js
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-amber-200 font-bold underline">Hello world!</h1>
+      <div className="test">
+        <h2 className="test_title">Test</h2>
+      </div>
+    </div>
+  );
+}
